ea5da1a2c4a57ef2a3d3c8432911aa5c638b3ad7
Mayank-Kumar <151028361+mayank-badliwal@users.noreply.github.com>
2024-11-23 18:09:42 +0000

    conainer config done

---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+// alert ( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code readability should be high
+
+console.log("Hitesh")
+
+// Datatypes 
+let name = "Mayank"
+let id = 32
+let isLoggedIn = true
+let state;
+
+/*
+number => 2 to the power 53
+bigint =>
+tring => " "
+null =>
+undefined => not assigned yet
+*/ 
