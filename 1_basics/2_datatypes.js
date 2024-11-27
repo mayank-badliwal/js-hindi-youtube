ff62f2448421c7693a8fdacce634d54f88f202d4
Mayank-Kumar <151028361+mayank-badliwal@users.noreply.github.com>
2024-11-27 10:20:10 +0000

    datatypes

--- 1_basics/2_datatypes.js
+++ 1_basics/2_datatypes.js
@@ -5,19 +5,28 @@ console.log(3
     + 
     3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Mayank")
 
 // Datatypes 
 let name = "Mayank"
 let id = 32
 let isLoggedIn = true
 let state;
-kbfkk
+
 /*
 number => 2 to the power 53
 bigint =>
 string => " "
 null => standalone value
 undefined => not assigned yet
+symbol => unique
+
+object
+
 
 */ 
+// console.log(typeof name)
+console.log(id)
+console.log(typeof null)    //object
+console.log(typeof undefined);    //undefined
+
