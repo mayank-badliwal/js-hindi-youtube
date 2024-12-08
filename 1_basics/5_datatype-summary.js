262384009cd92db76dc55c2e9bd83c8f29291863
Mayank-Kumar <151028361+mayank-badliwal@users.noreply.github.com>
2024-12-08 08:48:12 +0000

    datatype-summary

---
+++ 1_basics/5_datatype-summary.js
@@ -0,0 +1,56 @@
+// Primitive
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.57
+
+const isLoggedIn = false
+const outsideTemp = null
+let userMail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId)
+
+const bigNumber = 343556758948493n
+// console.log(typeof(bigNumber))
+
+// Refrence type(Non-primitive)
+// Array, Objects, functions
+
+//Array
+const heros = ["shaktimaan", "naagraaj", "doga"]
+// console.log(heros) 
+
+//Objects
+let myObj = {
+    name: "mayank",
+    age: 22,
+    city: "chandpur"
+}
+// console.log(myObj)
+
+//functions
+const myFunc = function() {
+    console.log("hello"); 
+}
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+/*
+    Value                                       Type      
+
+1.  String                                      "string"
+2.  Number                                      "number"
+3.  Null                                        "object"
+4.  Boolean                                     "boolean"
+5.  Object (native and 
+    does not implement                          "object"
+    [[Call]])	
+6.  Object (native or
+    host and does implement [[Call]])	        "function"
+7.  Object (host and 
+    does not implement [[Call]])	            Implementation-defined except may not be "undefined", "boolean", "number", or "string".
+*/
+
