83884f0b9e9b27f273de3e21885be86869c033d1
Mayank-Kumar <151028361+mayank-badliwal@users.noreply.github.com>
2024-12-24 17:48:51 +0000

    nums and maths

---
+++ 1_basics/6_Nums_n_math.js
@@ -0,0 +1,15 @@
+const number = 400
+// console.log(number)
+
+const number2 = new Number(100.2546)
+// console.log(number2)
+
+// console.log(number2.toString())
+// console.log(number2.toFixed(2))     // it is used to take numbers to a specific position after the decimal.
+
+// console.log(number2.toPrecision(4))     // it is used to print the round off value after the decimal.
+
+const number3 = 1002367
+// console.log(number3.toLocaleString('en-IN'))        // it is used to print a long number in any country standards, (en-IN) is used for INDIA, btw by-default it is in US standards.
+
+// ******************** Maths **********************
