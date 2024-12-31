cda9c1c4cb982574baa759423fae8a89ee5aea45
Mayank-Kumar <151028361+mayank-badliwal@users.noreply.github.com>
2024-12-31 14:12:45 +0000

    array

---
+++ 2_basics/1_arrays.js
@@ -0,0 +1,19 @@
+const myArr = [1, 2, 3, 4, 5]
+// console.log(myArr)
+
+// console.log(myArr[2])
+
+// myArr.push(6)    //it'll add in the end
+// myArr.pop()      //it'll remove from the end
+
+// myArr.unshift(0)    //it'll add in the start
+// myArr.shift()        //it'll remove from the start
+
+// console.log(myArr.includes(3))
+// console.log(myArr.indexOf(3))
+
+// const newArr = myArr.join()     // join() will convert array to string.
+// console.log(newArr)
+
+
+console.log(myArr)
