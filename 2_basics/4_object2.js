5dcad9f04f90986d46ad8209f44751c27c7be237
Mayank-Kumar <151028361+mayank-badliwal@users.noreply.github.com>
2025-03-04 18:42:23 +0000

    object2

---
+++ 2_basics/4_object2.js
@@ -0,0 +1,19 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "12345"
+tinderUser.name = "mayank"
+tinderUser.ratings = 4.5
+// console.log(tinderUser)
+
+const regularUser = {
+    email: "mayank@gmail.com",
+    fullName : {
+        firstName : "Mayank",
+        middleName : "kumar",
+        lastName : "thakur"
+    }
+}
+// console.log(regularUser.fullName.lastName);
+
